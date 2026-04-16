a26570326413e1ddfe55f1639cea1aea59ab5d2a
Chiru <chiranthofficial25@gmail.com>
2026-04-16 22:37:31 +0530

    Fixed pylint issues

--- calculator.py
+++ calculator.py
@@ -1,5 +1,11 @@
+"""Calculator functions."""
+
+
 def add(a, b):
+    """Return addition of two numbers."""
     return a + b
 
+
 def subtract(a, b):
+    """Return subtraction of two numbers."""
     return a - b
